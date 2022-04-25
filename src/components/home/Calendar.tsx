d7946fba587308474e658a7c4b462b2157c55a66
iarmankhan <work.armankhan@gmail.com>
2022-04-25 16:53:31 +0530

    Added functionality to switch selected week range using next & previous

--- src/components/home/Calendar.tsx
+++ src/components/home/Calendar.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import {View} from 'react-native';
 import {Calendar as RNCalendar} from 'react-native-calendars';
-import {Button, makeStyles, useTheme} from 'react-native-elements';
+import {Button, makeStyles} from 'react-native-elements';
 import moment from 'moment'
+import {theme} from "../../theme";
 
 interface CalendarProps {
 }
 
+const getMarkedDates = (weekNumber: number) => {
+    const obj: Record<string, any> = {
+        [moment('2022').add(weekNumber, 'weeks').startOf('week').format('YYYY-MM-DD')]: {selected: true, color: theme.colors?.primary, startingDay: true},
+    };
+
+    for(let i = 1; i <= 5; i++){
+        obj[moment('2022').add(weekNumber, 'weeks').startOf('week').add(i, 'days').format('YYYY-MM-DD')] = {selected: true,color: theme.colors?.primary};
+    }
+
+    obj[moment('2022').add(weekNumber, 'weeks').startOf('week').add(6, 'days').format('YYYY-MM-DD')] = {selected: true, color: theme.colors?.primary, endingDay: true};
+
+    return obj
+}
+
 const Calendar: React.FC<CalendarProps> = () => {
-    const {theme} = useTheme()
     const styles = useStyles();
 
+    const [selectedWeek, setSelectedWeek] = React.useState(+moment().format('w'));
     const [selected, setSelected] = React.useState<{
 
     }>(() => {
-        const obj: Record<any, any> = {
-            [moment().startOf('week').format('YYYY-MM-DD')]: {selected: true, color: theme.colors?.primary, startingDay: true},
-        };
-
-        for(let i = 1; i <= 5; i++){
-            obj[moment().startOf('week').add(i, 'days').format('YYYY-MM-DD')] = {selected: true,color: theme.colors?.primary};
-        }
-
-        obj[moment().startOf('week').add(6, 'days').format('YYYY-MM-DD')] = {selected: true, color: theme.colors?.primary, endingDay: true};
-
-        return obj
+        return getMarkedDates(+moment().format('w'));
     });
+
+
 
     return <View style={styles.container}>
         <RNCalendar
@@ -34,8 +41,14 @@ const Calendar: React.FC<CalendarProps> = () => {
         />
 
         <View style={styles.buttonsContainer}>
-            <Button  title='Previous' />
-            <Button title='Next' />
+            <Button  title='Previous' onPress={() => {
+                            setSelected(getMarkedDates(selectedWeek - 1));
+                setSelectedWeek(selectedWeek - 1);
+            }}/>
+            <Button title='Next' onPress={() => {
+                setSelected(getMarkedDates(selectedWeek + 1));
+                setSelectedWeek(selectedWeek + 1);
+            }} />
         </View>
     </View>;
 };
